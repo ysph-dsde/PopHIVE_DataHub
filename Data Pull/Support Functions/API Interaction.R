b16d93d21b9b8564adb38166bed96e1e8c3d03a3
Shelby Golden <sgolde13@alumni.jh.edu>
2025-04-09 12:51:00 -0400

    Reorganize simulation. Adjust saveParquet() for empty directories.

--- Data Pull/Support Functions/API Interaction.R
+++ Data Pull/Support Functions/API Interaction.R
@@ -37,11 +37,11 @@ suppressPackageStartupMessages({
 ## ----------------------------------------------------------------
 ## FROM "fileCache.R"
 
-timeStamp <- function(storeName, basepath=".", goalDate = lubridate::now()) {
+timeStamp <- function(storeIn, basepath=".", goalDate = lubridate::now()) {
   # Previously was mostRecentTimestamp() and retrievePath() used by retrieveRDS().
   # 
   #  "basepath": path location following the getwd() result.
-  # "storeName": archive directory name, immediately following basepath.
+  #   "storeIn": archive directory name, immediately following basepath.
   # 
   # This function lists all of the dates a source was archived, and will
   # summarize the most recently received archive per source type detected.
@@ -50,8 +50,8 @@ timeStamp <- function(storeName, basepath=".", goalDate = lubridate::now()) {
   assertthat::assert_that(any("POSIXct" %in% class(goalDate)))
   
   
-  # Construct the path to the folder where all copies of "storeName" are stored.
-  fullpath <- file.path(basepath, storeName)
+  # Construct the path to the folder where all copies of "storeIn" are stored.
+  fullpath <- file.path(basepath, storeIn)
   
   # Confirm directory at the provided path exists.
   if (!dir.exists(fullpath)){
@@ -128,14 +128,14 @@ timeStamp <- function(storeName, basepath=".", goalDate = lubridate::now()) {
 
 
 
-storeParquet <- function(obj, source, storeName, basepath='.', tolerance=24) {
+storeParquet <- function(obj, source, storeIn, basepath='.', tolerance=24) {
   # Previously was storeRDS().
   # 
   #       "obj": the data recently queried from the API into memory.
   #    "source": the name of the source. Recommend using standard naming
   #              schemes to better manage archive. One "source" for one API.
   #  "basepath": path location following the getwd() result.
-  # "storeName": archive directory name, immediately following basepath.
+  #   "storeIn": archive directory name, immediately following basepath.
   # "tolerance": the difference (in hours) between the nearest recorded
   #              archival record for the same source.
   # 
@@ -149,8 +149,8 @@ storeParquet <- function(obj, source, storeName, basepath='.', tolerance=24) {
     stop(sprintf("Basepath '%s' does not exist. Cannot write file.", basepath))
   }
   
-  # Construct the path to the folder where all copies of "storeName" are stored.
-  fullpath <- file.path(basepath, storeName)
+  # Construct the path to the folder where all copies of "storeIn" are stored.
+  fullpath <- file.path(basepath, storeIn)
   
   # Confirm directory at the provided path exists.
   if (!dir.exists(fullpath)){
@@ -158,39 +158,45 @@ storeParquet <- function(obj, source, storeName, basepath='.', tolerance=24) {
   }
   
   
-  # Prepare the file details.
-  time_now  <- Sys.time()
-  name      <- format(time_now, paste(source, format="%Y_%m_%d_%H_%M.parquet", sep = "_"))
-  writepath <- file.path(basepath, storeName, name)
+  if (nrow(mostRecent) == 0){
+    message(sprintf("No existing files present in directory '%s'.", fullpath))
   
-  
-  # Check that a file with that exact name is not going to be overwritten.
-  # Get all the files in this directory.
-  dirListing <- list.files(fullpath)
-  
-  # If there were no files in the directory, throw an error.
-  if (name %in% dirListing){
-    stop(sprintf("A file for '%s' has already been recorded.", source))
+  } else{
+    # If there are existing files, ensure that it is within tolerance.
+    
+    # Prepare the file details.
+    time_now  <- Sys.time()
+    name      <- format(time_now, paste(source, format="%Y_%m_%d_%H_%M.parquet", sep = "_"))
+    writepath <- file.path(basepath, storeIn, name)
+    
+    
+    # Check that a file with that exact name is not going to be overwritten.
+    # Get all the files in this directory.
+    dirListing <- list.files(fullpath)
+    
+    # If there were no files in the directory, throw an error.
+    if (name %in% dirListing){
+      stop(sprintf("A file for '%s' has already been recorded.", source))
+    }
+    
+    
+    # Even if the file name is not the exact same to the minute as a file
+    # that exists in the record, we don't want to save a record within a
+    # specified tolerance. This is set to 24hrs by default.
+    
+    proximity <- timeStamp(storeIn, basepath)$`Record History` %>%
+      filter(Source == source) %>%
+      # Find the difference between the nearest date and system time, 
+      # rounded to hours.
+      mutate(Delta = abs( purrr::map_dbl(History, ~lubridate::int_length(. %--% time_now))/3600 )) %>%
+      filter(Delta < 24) %>%
+      nrow()
+    
+    # If the newly queried file is within the tolerance level, throw an error.
+    if (proximity != 0){
+      stop(sprintf("A file for '%s' was recorded within the tolerance range.", source))
+    }
   }
-  
-  
-  # Even if the file name is not the exact same to the minute as a file
-  # that exists in the record, we don't want to save a record within a
-  # specified tolerance. This is set to 24hrs by default.
-  
-  proximity <- timeStamp(storeName, basepath)$`Record History` %>%
-    filter(Source == source) %>%
-    # Find the difference between the nearest date and system time, 
-    # rounded to hours.
-    mutate(Delta = abs( purrr::map_dbl(History, ~lubridate::int_length(. %--% time_now))/3600 )) %>%
-    filter(Delta < 24) %>%
-    nrow()
-  
-  # If the newly queried file is within the tolerance level, throw an error.
-  if (proximity != 0){
-    stop(sprintf("A file for '%s' was recorded within the tolerance range.", source))
-  }
-  
   
   # If file creation conditions are met, then save as a new parquet file.
   write_parquet(obj, writepath)
@@ -204,33 +210,136 @@ storeParquet <- function(obj, source, storeName, basepath='.', tolerance=24) {
 ## ----------------------------------------------------------------
 ## FROM "runIfExpired.R"
 
-runIfExpired <- function(source, storeName, basepath, f, tolerance=24) {
+runIfExpired <- function(source, storeIn, f, tolerance = 24, return_recent = FALSE, basepath = "Data Pull") {
+  source <- "rsv-net"
+  storeIn <- "RESP NET Archive"
+  basepath <- "Data Pull"
+  f <- ~ read.socrata(url_rsv_net)
+  tolerance = 24
+  return_recent = FALSE
+  
+  
   api_function <- rlang::as_function(f)
   
-  mostRecent <- timeStamp(storeName, basepath)$`Report Relative to Date` %>%
+  mostRecent <- timeStamp(storeIn, basepath)$`Report Relative to Date` %>%
     filter(Source == source, Status %in% c("Recent Pull", "Both"))
 
   data <- api_function()
   
   
   if ( nrow(mostRecent) == 0 ) {
-    # Here is where the cross-over check will be done. Save the appended
-    # result that is read in memory.
+    rm_recent_null()
     
     message(sprintf("NEW dataset for '%s' in this directory.", source))
-    return(storeParquet(data, source, storeName, basepath, tolerance))
+    return(storeParquet(data, source, storeIn, basepath, tolerance))
     
-  } else if( mostRecent$History %--% now() < hours(tolerance) ){
+  } else if(return_recent == TRUE & mostRecent$History %--% now() < hours(tolerance) ){
     message(sprintf("OLD dataset for '%s' is being called to memory. Save query is within the tolerance level.", source))
-    return(read_parquet( file.path(basepath, storeName, mostRecent$filePath) ))
+    return(read_parquet( file.path(basepath, storeIn, mostRecent$filePath) ))
+    
+  } else if(return_recent == FALSE & mostRecent$History %--% now() < hours(tolerance)) {
+    message(sprintf("Request is too close to the most recent archive '%s'.", mostRecent$filePath))
+    return(NULL)
     
   } else{
     message(sprintf("Added another dataset for '%s' in this directory.", source))
-    return(storeParquet(data, source, storeName, basepath, tolerance))
+    return(storeParquet(data, source, storeIn, basepath, tolerance))
     
   }
   
 }
+
+
+
+
+# -----------------------------
+# Remove missing entries from recently dated entries.
+# 
+# Developed in "Nuanced Pull Simulation/Testing Script.R"
+
+rm_recent_null <- function(data, date_col, dictionary){
+  # Some sources will add rows that are recent to the API call, but lack
+  # any value entries. They're recorded as "NULL" or NA's. This function
+  # will remove recently dated entries that have no value entered. It
+  # references the Data Dictionary to find rows in the raw format that
+  # are labeled as "Outcome", and uses all of these to search over.
+  #
+  #       "data": the data that was recently called in by the API.
+  #   "date_col": specify the column with whole date associated. 
+  #               with the data entry. Needs to be class type "Date".
+  # "dictionary": the CSV containing information about the raw data
+  #               columns that are expected.
+  
+  
+  # Pull the column names and their use assignment from the data dictionary.
+  col_type = raw_columns(dictionary)
+  # Isolate the variables that are used for "Outcome". These are the
+  # numeric columns of interest.
+  variables <- col_type[col_type$Use == "Outcome", "Raw Data Columns"]
+  
+  filter_statement <- variables %>%
+    str_c(., " %in% c('NULL', NA)") %>%
+    str_flatten(collapse = " & ")
+  
+  # Confirm that date_col is a Date object.
+  assertthat::assert_that(any("Date" %in% sapply(data[, colnames(data) %in% date_col], class)))
+  
+  
+  # -----------------------------
+  # Find and remove the recent date range that has NA's and NULL.
+  
+  # Group dates that are consecutive weeks.
+  dates_na_null <- data %>%
+    # Filter over the outcome columns.
+    filter(eval(rlang::parse_expr(filter_statement))) %>%
+    # Save and order the dates associated.
+    select(week_ending_date) %>% pull() %>% unique() %>% sort() %>%
+    # Evaluate which dates are not within 7 days of each other and
+    # save in separate list entries.
+    (\(x) { split(x, cumsum(c(TRUE, diff(x) != 7))) }) ()
+  
+  
+  recent_date <- data[, colnames(data) %in% date_col] %>% pull() %>% max()
+  
+  # The list entry that represents the most recent set of entries.
+  eval_recent_dates <- dates_na_null %>%
+    # Search each list element for the max year in the consecutive
+    # series of weeks.
+    lapply(., function(x){
+      any(x %in% recent_date)
+    }) %>%
+    unlist() %>% 
+    # Report the index of the weeks that are most recent.
+    (\(x) { which(x == TRUE) }) ()
+  
+  
+  # Confirm the recent date of entry is included in the recent date
+  # ranges where NULL and NA's were found for all variables. If not,
+  # exit the function.
+  if(length(eval_recent_dates) < 1){
+    stop( sprintf("Most recent entries where %s have NULL or NA's does not include \nthe most recent date. Step not needed; abort operation.", str_flatten(variables, collapse = " and ")) )
+  }
+  
+  # Create an argument that will filter out the recent dates with
+  # no reported values.
+  date_filter_argument <- 
+    str_flatten(dates_na_null[[eval_recent_dates]], collapse = "', '") %>%
+    str_c("c('", ., "')") %>%
+    paste(date_col, ., sep = " %!in% ")
+  
+  # Remove these values.
+  result <- data %>% 
+    filter(eval(rlang::parse_expr(date_filter_argument))) %>%
+    `rownames<-`(NULL)
+  
+  
+  result
+  
+}
+
+
+
+
 
 
 
