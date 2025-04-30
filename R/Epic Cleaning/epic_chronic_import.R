9a8de2740f3df89229b8b6edcea1218ee218e1ed
weinbergerlab <dweinber@gmail.com>
2025-04-30 16:40:45 -0400

    adds the population size to the diabetes/obesity file

--- R/Epic Cleaning/epic_chronic_import.R
+++ R/Epic Cleaning/epic_chronic_import.R
@@ -5,15 +5,25 @@
 epic_import_chronic <- function(ds_name, skipN=12) {
   
   ds_out <- readr::read_csv(ds_name, skip=skipN, col_names=T) %>%
-    rename(geography=`State of Residence (U.S.)`, age=`Age in Years (Current)`,  pct="...4") %>%
-    tidyr::fill( Measures,geography, age,  .direction = 'down') %>%
-    dplyr::select(Measures,geography, age, pct )%>%
+    rename(geography=`State of Residence (U.S.)`, age=`Age in Years (Current)`,  
+           Number_Patients="...3",
+           pct_diabetes ="...4",
+           pct_obesity ="...5",
+           
+           ) %>%
+    tidyr::fill( geography, age,  .direction = 'down') %>%
+    dplyr::select(geography, age, Number_Patients,pct_diabetes,pct_obesity )%>%
     arrange(geography, age) %>%
     #week END date
     ungroup() %>%
-    mutate( pct = if_else(pct=='-',NA_character_, pct),
-            pct=gsub('%','', pct),
-            pct = as.numeric(pct),
+    mutate( pct_diabetes = if_else(pct_diabetes=='-',NA_character_, pct_diabetes),
+            pct_diabetes=gsub('%','', pct_diabetes),
+            pct_diabetes = as.numeric(pct_diabetes),
+            
+            pct_obesity = if_else(pct_obesity=='-',NA_character_, pct_obesity),
+            pct_obesity=gsub('%','', pct_obesity),
+            pct_obesity = as.numeric(pct_obesity),
+            
             geography= if_else(geography=='Total', 'United States', geography)) %>%
     mutate( Level = if_else(toupper(age) %in% toupper(c('Less than 10 Years')), '<10 Years',
                             if_else( toupper(age) %in%  toupper(c('≥ 10 and < 15 Years','? 10 and < 15 years')),'10-14 Years',
@@ -23,12 +33,10 @@ epic_import_chronic <- function(ds_name, skipN=12) {
                                                                if_else( toupper(age) %in% toupper(c('65 years or more')) , "65+ Years" , 
                                                                         if_else(age=='Total','Total',NA_character_        
                                                                                 
-                                                                        ) )))))),
-            Measures = if_else(grepl('BMI',Measures),'obesity',
-                               if_else(grepl('A1C',Measures) , 'diabetes','none' ))
+                                                                        ) ))))))
     ) %>%
     dplyr::select(-age) %>%
-    arrange(Measures, geography,Level) %>%
+    arrange( geography,Level) %>%
     filter(!is.na(geography))
   
   return(ds_out)
