44be8985820707e757537bb924db518316930f54
Weinberger <daniel.weinberger@yale.edu>
2025-05-02 22:12:49 -0400

    requested files

---
+++ R/Support Functions/Webslim_files.R
@@ -0,0 +1,173 @@
+##Take the previous 'plot files' format and slim down for the final
+#Dataface website
+
+library(tidyverse)
+library(arrow)
+
+url_files <- 'https://raw.githubusercontent.com/ysph-dsde/PopHIVE_DataHub/refs/heads/main/Data/Plot%20Files/'
+
+
+#######################################
+## RSV
+#######################################
+
+#NREVSS data
+
+nrevss_rsv_ts <- read_csv(paste0(url_files,'NREVSS/rsv_ts_nrevss_test_rsv.csv')) %>%
+  filter(!is.na(hhs_abbr)) %>%
+  dplyr::select(hhs_abbr, MMWRweek, MMWRyear,pcr_detections) %>%
+  rename(geography=hhs_abbr, week=MMWRweek, year=MMWRyear,value=pcr_detections)
+
+write_parquet(nrevss_rsv_ts,'./Data/Webslim/respiratory_diseases/rsv/positive_tests.parquet')
+
+##RSV all indicators, all ages
+
+rsv_all_indicators_state <- read_csv(paste0(url_files,'Comparisons/rsv_combined_all_outcomes_state.csv')) %>%
+  filter(outcome_label1 != 'Google Searches 1') %>% #only keep definition #2 for google searches
+  dplyr::select(geography, date, source, Outcome_value1) %>%  #note this is RAW data; needs 3 week ave and scaling for plot
+  rename(value=Outcome_value1) %>%
+  mutate( source = if_else(source=="Epic Cosmos" , "Epic Cosmos, ED",source ),
+          source = if_else(source=="CDC RSV-NET (RespNet)" , "CDC RSV-NET",source ))
+
+write_parquet(rsv_all_indicators_state,'./Data/Webslim/respiratory_diseases/rsv/overall_trends.parquet')
+
+
+##RSV Cosmos age and RSV-Net by age
+epic_ed_combo_rsv <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_epic_cosmos_age_state.csv')) %>%
+  mutate(source= "Epic Cosmos (ED)") %>%
+  filter(outcome_name=='RSV') %>%
+    dplyr::select(date, geography, age_level, source, Outcome_value1) %>%
+  rename(value=Outcome_value1)
+
+rsvnet_age <- read_csv(paste0(url_files,'RESP-NET%20Programs/rsv_hosp_age_respnet.csv')) %>%
+  mutate(source= "CDC RSV-NET (Hospitalization)") %>%
+  dplyr::select(date, state, Level, source, hosp_rate) %>%
+  rename(value=hosp_rate,
+         geography=state,
+         age=Level)
+
+age_trends_rsv <- bind_rows(epic_ed_combo_rsv,rsvnet_age)
+
+write_parquet(age_trends_rsv,'./Data/Webslim/respiratory_diseases/rsv/trends_by_age.parquet')
+
+##RSV by county and week
+
+rsv_county <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_county_filled_map_nssp.csv')) %>%
+  dplyr::select(fips,week_end,percent_visits_rsv)
+
+write_parquet(rsv_county,'./Data/Webslim/respiratory_diseases/rsv/ed_visits_by_county.parquet')
+
+
+######################################
+##INFLUENZA
+######################################
+
+##flu all indicators, all ages
+
+flu_all_indicators_state <- read_csv(paste0(url_files,'Comparisons/flu_combined_all_outcomes_state.csv')) %>%
+  filter(outcome_label1 != 'Google Searches 1') %>% #only keep definition #2 for google searches
+  dplyr::select(geography, date, source, Outcome_value1) %>%  #note this is RAW data; needs 3 week ave and scaling for plot
+  rename(value=Outcome_value1) %>%
+  mutate( source = if_else(source=="Epic Cosmos" , "Epic Cosmos, ED",source ))
+
+write_parquet(flu_all_indicators_state,'./Data/Webslim/respiratory_diseases/influenza/overall_trends.parquet')
+
+
+##flu Cosmos age 
+epic_ed_combo_flu <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_epic_cosmos_age_state.csv')) %>%
+  mutate(source= "Epic Cosmos (ED)") %>%
+  filter(outcome_name=='FLU') %>%
+  dplyr::select(date, geography, age_level, source, Outcome_value1) %>%
+  rename(value=Outcome_value1)
+
+write_parquet(epic_ed_combo_flu,'./Data/Webslim/respiratory_diseases/influenza/trends_by_age.parquet')
+
+
+##flu by county and week
+
+flu_county <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_county_filled_map_nssp.csv')) %>%
+  dplyr::select(fips,week_end,percent_visits_flu)
+
+write_parquet(flu_county,'./Data/Webslim/respiratory_diseases/influenza/ed_visits_by_county.parquet')
+
+
+######################################
+##COVID
+######################################
+
+##covid all indicators, all ages
+
+covid_all_indicators_state <- read_csv(paste0(url_files,'Comparisons/covid_combined_all_outcomes_state.csv')) %>%
+  filter(outcome_label1 != 'Google Searches 1') %>% #only keep definition #2 for google searches
+  dplyr::select(geography, date, source, Outcome_value1) %>%  #note this is RAW data; needs 3 week ave and scaling for plot
+  rename(value=Outcome_value1) %>%
+  mutate( source = if_else(source=="Epic Cosmos" , "Epic Cosmos, ED",source ))
+
+write_parquet(covid_all_indicators_state,'./Data/Webslim/respiratory_diseases/covid/overall_trends.parquet')
+
+
+##flu Cosmos age 
+epic_ed_combo_covid <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_epic_cosmos_age_state.csv')) %>%
+  mutate(source= "Epic Cosmos (ED)") %>%
+  filter(outcome_name=='COVID') %>%
+  dplyr::select(date, geography, age_level, source, Outcome_value1) %>%
+  rename(value=Outcome_value1)
+
+write_parquet(epic_ed_combo_covid,'./Data/Webslim/respiratory_diseases/covid/trends_by_age.parquet')
+
+
+##flu by county and week
+
+covid_county <- read_csv(paste0(url_files,'Cosmos%20ED/rsv_flu_covid_county_filled_map_nssp.csv')) %>%
+  dplyr::select(fips,week_end,percent_visits_covid)
+
+write_parquet(covid_county,'./Data/Webslim/respiratory_diseases/covid/ed_visits_by_county.parquet')
+
+
+#################################################################
+## Pneumococcus
+################################################################
+
+#IPD serotype trends
+ipd_serotype_age <- read_csv(paste0(url_files,'./pneumococcus/ipd_serotype_age_year.csv')) %>%
+  dplyr::select(st, agec2, year, N_IPD) %>%
+  rename(serotype=st,
+         age=agec2,
+         value=N_IPD)
+
+write_parquet(ipd_serotype_age,'./Data/Webslim/respiratory_diseases/pneumococcus/serotype_trends.parquet')
+
+
+#Geographic variation (based on genomic data)
+
+ipd_serotype_state <- read_csv(paste0(url_files,'./pneumococcus/ipd_serotype_state_pct.csv')) %>%
+  dplyr::select(sero, State, pct) %>%
+  rename(serotype=sero, geography=State, value=pct)
+
+write_parquet(ipd_serotype_state,'./Data/Webslim/respiratory_diseases/pneumococcus/by_geography.parquet')
+
+
+##comparison of IPD and pneumonia
+
+ipd1 <- readRDS(url('https://github.com/ysph-dsde/PopHIVE_DataHub/raw/refs/heads/main/Data/Pulled%20Data/pneumococcus/ABCs_st_1998_2023.rds')) %>%
+  rename(agec = "Age.Group..years.",
+         year=Year,
+         st=IPD.Serotype,
+         N_IPD = Frequency.Count) %>%
+  mutate( st= if_else(st=='16','16F', st),
+          st = if_else(st %in% c('15B','15C'), '15BC',st),
+          if_else(st %in% c('6A','6C'), '6AC',st)
+  ) %>%
+  filter(year %in% c(2019,2020) & agec %in% c('Age 50-64','Age 65+')) %>%
+  group_by(st) %>%
+  summarize( N_IPD= sum(N_IPD))
+
+uad <- read_csv('https://github.com/ysph-dsde/PopHIVE_DataHub/raw/refs/heads/main/Data/Pulled%20Data/pneumococcus/ramirez_ofid_2025_ofae727.csv') %>%
+  mutate(N_SSUAD= over65 + a50_64_with_indication + a50_64_no_indication ) %>%
+  full_join(ipd1, by='st') %>%
+  filter(!is.na(N_SSUAD) & !is.na(N_IPD)) %>%
+  dplyr::select(st, N_SSUAD, N_IPD) %>%
+  rename(serotype=st, ipd=N_IPD, pneumonia=N_SSUAD )
+
+write_parquet(uad,'./Data/Webslim/respiratory_diseases/pneumococcus/comparison.parquet')
+
