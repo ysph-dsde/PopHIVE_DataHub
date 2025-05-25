d3069482c5868111bcbc1c8fcda8c8cd64cf0b29
Weinberger <daniel.weinberger@yale.edu>
2025-05-25 04:09:56 -0400

    add exemption data

--- R/Data Pull/Data Pull_school_vax_view.R
+++ R/Data Pull/Data Pull_school_vax_view.R
@@ -9,5 +9,18 @@
 #write_parquet(a1, './Data/Pulled Data/vax/school_vax_view.parquet' )
 
 
-#a1 <- read_parquet('./Data/Pulled Data/vax/school_vax_view.parquet')
+a1 <- read_parquet('./Data/Pulled Data/vax/school_vax_view.parquet')
 
+exemptions <- a1 %>%
+  filter(grepl('Exemption',dose)) %>%
+  mutate( vax = if_else(dose=='Any Exemption', 'full_exempt',
+                        if_else(dose=='Medical Exemption', 'medical_exempt',
+                                if_else(dose=='Non-Medical Exemption', 'personal_exempt',
+                                        NA_character_
+                        ))),
+          grade='Kindergarten') %>%
+  rename(year=year_season,N=population_sample_size , value=coverage_estimate) %>%
+  dplyr::select(year, geography, grade,N, vax,value, percent_surveyed)
+
+write.csv(exemptions,'./Data/Plot Files/childhood_immunizations/exemption_state_kg_school_vax_view.csv')
+write_parquet(exemptions,'./Data/Webslim/childhood_immunizations/exemption_state_kg_school_vax_view.parquet')
