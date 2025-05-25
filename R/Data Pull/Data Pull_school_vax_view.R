97ab8d1695cdbf2edf01f056c857aed7e9814ffc
Weinberger <daniel.weinberger@yale.edu>
2025-05-25 03:48:54 -0400

    placeholder file for school vax view import

---
+++ R/Data Pull/Data Pull_school_vax_view.R
@@ -0,0 +1,13 @@
+#https://data.cdc.gov/resource/ijqb-a7ye.csv
+
+
+#Kindergarten vaccine uptake from SchoolVaxView.
+#Surveys of schools; methodology and completeness vary by state
+#https://data.cdc.gov/resource/ijqb-a7ye.csv
+
+#a1 <- read.csv('https://data.cdc.gov/resource/ijqb-a7ye.csv')
+#write_parquet(a1, './Data/Pulled Data/vax/school_vax_view.parquet' )
+
+
+#a1 <- read_parquet('./Data/Pulled Data/vax/school_vax_view.parquet')
+
